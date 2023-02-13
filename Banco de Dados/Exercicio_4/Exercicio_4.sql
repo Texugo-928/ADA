fd0b5356e576acb2fe953cebdf0d01065275fd46
Cicero V. de Melo Jr <108836762+Texugo-928@users.noreply.github.com>
2023-02-12 22:35:23 -0300

    Adiciona Banco de Dados v1.7

---
+++ Banco de Dados/Exercicio_4/Exercicio_4.sql
@@ -0,0 +1,24 @@
+USE Northwind;
+
+--Visualizando as tabelas:
+--Categories, CustomerCustomerDemo, CustomerDemographics, Customers, Employees,
+--EmployeeTerritories, [Order Details], Orders, Products, Region, Shippers,
+--Suppliers, Territories
+
+SELECT * FROM Categories;
+SELECT * FROM CustomerCustomerDemo;
+SELECT * FROM CustomerDemographics;
+SELECT * FROM Customers;
+SELECT * FROM Employees;
+SELECT * FROM EmployeeTerritories;
+SELECT * FROM [Order Details];
+SELECT * FROM Orders;
+SELECT * FROM Products;
+SELECT * FROM Region;
+SELECT * FROM Shippers;
+SELECT * FROM Suppliers;
+SELECT * FROM Territories;
+
+--Trabalhando com essas funções:
+--MIN(), MAX(), COUNT(*), SUM(), AVG(), HAVING
+
