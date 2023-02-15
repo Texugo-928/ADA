09aa9eb6ed22cf997cd2903177efdc05a1fac5df
Cicero V. de Melo Jr <108836762+Texugo-928@users.noreply.github.com>
2023-02-14 23:12:39 -0300

    .

--- Banco de Dados/Projeto/CRUD/Create/InsertsTipoProdutos.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO TipoProdutos VALUES
-('Eletronicos'), -- Notebook, celular, Computador, tablet
-('Veiculos'), -- Carro, caminhão, moto, frota...
-('Pessoais'), -- De vida, de acidentes, de viagem, patrimonial 
-('Residencial') -- Escritório, casa, apartamento, prédio, fazenda
